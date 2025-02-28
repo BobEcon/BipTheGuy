1a63b8b3bf5b048c234f98bcffee7f8c4c756d8a
Bob Beachill <bbeachill@icloud.com>
2025-02-28 19:21:49 +0000

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Robert Beachill on 28/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
